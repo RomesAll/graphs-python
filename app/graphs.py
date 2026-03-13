bf1b187fdec9712c193a81c277c3bf2f5047d3e4
RomesAll <romesky845@gmail.com>
2026-03-13 10:39:03 +0300

    feat: добавил класс для Edge для хранения ребер узлов

--- app/graphs.py
+++ app/graphs.py
@@ -8,3 +8,11 @@ class Node:
         self.value: object = value
         self.edges: list[Node] = [] # ребра узла
         self.parents: list[Node] = [] # родители узла
+
+class Edge:
+    """
+    Класс для хранения ребер, версия с взвешанным и невзвешенный графом
+    """
+    def __init__(self, adjacent, weight: int=0):
+        self.weight: int = weight
+        self.adjacent: Node = adjacent
