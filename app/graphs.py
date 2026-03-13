c4e510cd1416093f4e15a60782a44beb93334e49
RomesAll <romesky845@gmail.com>
2026-03-13 12:32:22 +0300

    feat: добавил базовый абстрактный класс BaseGraph с базовыми методами

--- app/graphs.py
+++ app/graphs.py
@@ -1,3 +1,7 @@
+from abc import ABC, abstractmethod
+
+Matrix = list[list[int]]
+
 class Node:
     """
     Класс для хранения узлов графа
@@ -11,8 +15,116 @@ class Node:
 
 class Edge:
     """
-    Класс для хранения ребер, версия с взвешанным и невзвешенный графом
+    Класс для хранения ребер, версия с взвешенным и невзвешенный графом
     """
     def __init__(self, adjacent, weight: int=0):
         self.weight: int = weight
         self.adjacent: Node = adjacent
+
+class BaseGraph(ABC):
+    """
+    Абстрактный базовый класс для графа
+    """
+    def __init__(self, matrix: Matrix):
+        self.matrix: Matrix = matrix
+        self.graph: dict|None = None
+        self.create_graph(matrix)
+
+    def calculate_node(self) -> int:
+        """
+        Функция подсчета кол-ва вершин
+        :return:
+        """
+        pass
+
+    def calculate_edge(self) -> int:
+        """
+        Функция подсчета кол-ва ребер
+        :return:
+        """
+        pass
+
+    def searching_isolated_node(self) -> Node|list[Node]|None:
+        """
+         Функция поиска изолированной вершины
+         :return: Node: Найденная вершина(ы), либо None
+         """
+        pass
+
+    def searching_loop_node(self) -> Node|list[Node]|None:
+        """
+        Функция для поиска петли у вершин
+        :return: Node: Найденная вершина(ы), либо None
+        """
+        pass
+
+    def get_degree_all_nodes(self) -> list[Node]|None:
+        """
+        Функция для вывода всех степеней вершин в порядке убывания
+        :return: Node: Список вершин
+        """
+        pass
+
+    @abstractmethod
+    def create_graph(self, matrix: Matrix) -> dict[int, Node]:
+        """
+        Функция создания графа.\n
+        Для представления графа используется хеш-таблицы, списки, классы,
+        как наиболее эффективный способ хранения данных в памяти компьютера.
+
+        Входные данные: Двумерный массив один из двух вариантов:
+        \n Матрица смежности для невзвешенного графа
+        \n Пример: [[0,1], [1,0]];
+        \n Матрица с перечислением вершин и ребер для взвешенного графа
+        \n Пример: [[1,2,5], [2,1,9]], где:
+        - 0-ой индекс от какой вершины идем;
+        - 1-й индекс к какой вершине идем;
+        - 2-й индекс вес ребра.
+        :param matrix: Двумерный массив,
+        :return: dict[int, Node]: хеш-таблица, где ключ имя - вершина, а значение - объект узла Node
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def dfs(self, node: Node, passed: set):
+        """
+        Функция обхода графа в глубину, с помощью рекурсии
+        :param node: Узел начала
+        :param passed: Список узлов, которые мы прошли
+        :return: None
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def bfs(self, node: Node, passed: set):
+        """
+        Функция обхода графа в ширину, с помощью итеративного подхода
+        :param node: Узел начала
+        :param passed: Список узлов, которые мы прошли
+        :return: None
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_path(self, start: Node, end: Node, passed: set, path: list) -> bool:
+        """
+        Функция для поиска первого пути
+        :param start: Узел начала
+        :param end: Узел конца
+        :param passed: Список узлов, которые мы прошли
+        :param path: Список узлов найденного пути от начала до конца
+        :return: bool
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_app_path(self, start: Node, end: Node, passed: set, path: list[list]):
+        """
+        Функция для поиска всех путей
+        :param start: Узел начала
+        :param end: Узел конца
+        :param passed: Список узлов, которые мы прошли
+        :param path: Список узлов найденного пути от начала до конца
+        :return: bool
+        """
+        raise NotImplementedError
