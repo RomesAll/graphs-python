35d940ec2bc2678a943fc163f3ba695c5f54c54e
RomesAll <romesky845@gmail.com>
2026-03-14 15:09:18 +0300

    refactor(core): добавил импорты в init файл

---
+++ app/core/__init__.py
@@ -0,0 +1,8 @@
+from .domain_services import GraphRepository
+from .entities import Node, Edge
+from .services import GraphService
+__all__ = [
+    'GraphRepository',
+    'Node', 'Edge',
+    'GraphService',
+]
