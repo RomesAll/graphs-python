b5503c3869378d1838428d60001f5f2f8814bfc7
RomesAll <romesky845@gmail.com>
2026-03-15 18:23:05 +0300

    feat(domain_model): добавил метод у класса Edge, который проверяет что вершина это объект Node

--- app/core/entities/graph_objects.py
+++ app/core/entities/graph_objects.py
@@ -2,17 +2,22 @@ class Node:
     """
     Класс для хранения узлов графа
     """
-    # todo: Для хранения узлов в edges
-    #  и parents лучше всего использовать связные списки
+    # todo: Для хранения узлов в edges и parents лучше всего использовать связные списки
     def __init__(self, value: object):
         self.value: object = value
-        self.edges: list[Edge] = [] # ребра узла
-        self.parents: list[Node] = [] # родители узла
+        self.edges: list['Edge'] = [] # ребра узла
+        self.parents: list['Node'] = [] # родители узла
 
 class Edge:
     """
     Класс для хранения ребер, версия с взвешенным и невзвешенный графом
     """
-    def __init__(self, adjacent, weight: int=0):
+    def __init__(self, adjacent, weight: int = 0):
         self.weight: int = weight
-        self.adjacent: Node = adjacent
+        self.adjacent: Node | None = None
+        self.add_adjacent(adjacent)
+
+    def add_adjacent(self, node: 'Node'):
+        if not isinstance(node, Node):
+            raise TypeError()
+        self.adjacent = node
