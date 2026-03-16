21f5e509b74c3516e2acfea745a802983a78c0ac
RomesAll <romesky845@gmail.com>
2026-03-16 23:04:09 +0300

    feat: добавил вызов ConsoleUI() в main файле

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-from app.infrastructure.repository.graph.base import Graph
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+if __name__ == '__main__':
+    from app.infrastructure.user_interfaces.console.console import ConsoleUI
+    console = ConsoleUI()
+    console.start()
